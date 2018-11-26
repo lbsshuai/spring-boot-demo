1bd896bf65f9a139d51b4041911afcb38ed9a427
lbs13329 <liubingshuai@noah-fund.com>
2018-11-26 18:10:05 +0800

    日常更新

--- src/main/resources/static/js/cpts/single.js
+++ src/main/resources/static/js/cpts/single.js
@@ -1,23 +1,41 @@
 /**
  * 定义变量
  */
-
+var startSingleInfo = "";
 
 /**
  * 初始化
  */
 $(function () {
 
-
+    init();
 });
 
+function init() {
+    startSingle();
+}
+
+function startSingle() {
+    for(var i = 1; i < 5; i++){
+        startSingleInfo += "<li data-thumb=\"images/s"+i+".jpg\">"+
+        "<img src=\"/cpts/images/s"+i+".jpg\" /></li>"
+    }
+    console.log(startSingleInfo);
+    document.getElementById('startSingle').innerHTML = startSingleInfo;
+}
 
 
 
 
 
-
-
+/*let app = new Vue({
+    el : '#app',
+    data: {
+        list:["s1.jpg", "s2.jpg", "s3.jpg", "s4.jpg"],
+        list1:["images/s1.jpg", "images/s2.jpg", "images/s3.jpg", "images/s4.jpg"],
+        list2:["/cpts/images/s1.jpg", "/cpts/images/s2.jpg", "/cpts/images/s3.jpg", "/cpts/images/s4.jpg"]
+    }
+});*/
 
 
 
